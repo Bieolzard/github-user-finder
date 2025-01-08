30c34aa4914dbd15108da74b05c12d73164f476f
Gabriel Mateus de Andrade <saintseiyatcl@gmail.com>
2025-01-08 20:29:03 -0300

    chore: initial project setup with Vite, React, ChakraUI, and TypeScript

---
+++ github-profile-viewer/src/App.tsx
@@ -0,0 +1,17 @@
+// src/App.tsx
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        olá mundo
+      </div>
+    </Router>
+  );
+}
+
+export default App;
