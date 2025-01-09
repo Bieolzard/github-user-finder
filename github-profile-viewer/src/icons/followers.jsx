6939f3d1819e2e22e03731fc14ca3e8d23f621c6
Gabriel Mateus de Andrade <saintseiyatcl@gmail.com>
2025-01-09 16:20:35 -0300

    feat: Add icons and improving the ProfilePage

---
+++ github-profile-viewer/src/icons/followers.jsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+const followersIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    {...props}
+  >
+    <path
+      stroke="#4A5568"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      d="M20.793 9.524a2.023 2.023 0 1 1-2.862 2.86 2.023 2.023 0 0 1 2.861-2.86M14.203 5.912a3.115 3.115 0 1 1-4.406 4.406 3.115 3.115 0 0 1 4.406-4.406M6.068 9.524a2.023 2.023 0 1 1-2.86 2.86 2.023 2.023 0 0 1 2.86-2.86M23 19v-1.096a2.5 2.5 0 0 0-2.5-2.5h-.801M1 19v-1.096a2.5 2.5 0 0 1 2.5-2.5h.801M17.339 19v-1.601a3.5 3.5 0 0 0-3.5-3.5H10.16a3.5 3.5 0 0 0-3.5 3.5V19"
+    />
+  </svg>
+)
+export default followersIcon
--- github-profile-viewer/src/pages/Profile.tsx
+++ github-profile-viewer/src/pages/Profile.tsx
@@ -1,12 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Box, Button, Text, Image, Stack, Link, Spinner } from '@chakra-ui/react';
+import { Box, Button, Text, Image, Link, Spinner, HStack, VStack } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 import axiosInstance from '../axiosInstance'; // Importa a instância
 import { z } from 'zod';
-import { format } from 'date-fns'; // Para formatar a data
+import { formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale';  // Importando a localidade em português
 import InfiniteScroll from 'react-infinite-scroll-component'; // Importando o Infinite Scroll
 import Header from '../components/Header'; // Importando o Header
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-expect-error    
+import FollowersIcon from '../icons/followers';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-expect-error 
+import Twitter from '../icons/twitter'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-expect-error 
+import Site from '../icons/site'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-expect-error 
+import Company from '../icons/company'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-expect-error 
+import Heart from '../icons/heart'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-expect-error 
+import Loc from '../icons/location'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-expect-error 
+import Email from '../icons/email'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-expect-error 
+import StarIcon from '../icons/star.jsx'
 
 // Defina o modelo Zod para o usuário e os repositórios
 const userSchema = z.object({
@@ -16,13 +41,19 @@ const userSchema = z.object({
   bio: z.string().nullable(),
   blog: z.string().nullable(),
   twitter_username: z.string().nullable(),
+  followers: z.number(),
+  following: z.number(),
+  company: z.string().nullable(),
+  location: z.string().nullable(),
+  email: z.string().nullable(),
 });
 
 const repoSchema = z.object({
   name: z.string(),
   html_url: z.string(),
   stargazers_count: z.number(),  // Mudado para o campo correto
-  created_at: z.string(),
+  updated_at: z.string(),
+  description: z.string().nullable().optional(),
 });
 
 type User = z.infer<typeof userSchema>;
@@ -30,12 +61,12 @@ type Repo = z.infer<typeof repoSchema>;
 
 const Profile = () => {
   const { username } = useParams();
-  const [userData, setUserData] = useState<User | null>(null); // Tipagem correta para userData
-  const [repos, setRepos] = useState<Repo[]>([]); // Tipagem correta para repos
+  const [userData, setUserData] = useState<User | null>(null);
+  const [repos, setRepos] = useState<Repo[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
-  const [page, setPage] = useState(1); // Adicionando o estado da página
-  const [hasMore, setHasMore] = useState(true); // Verifica se ainda há mais repositórios
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
   const { t } = useTranslation();
 
   // Função para buscar os dados do usuário e repositórios
@@ -49,17 +80,15 @@ const Profile = () => {
       const userValidated = userSchema.parse(userResponse.data);
       const reposValidated = repoResponse.data.map((repo: unknown) => repoSchema.parse(repo));
 
-      // Atualiza o estado com os dados validados
       setUserData(userValidated);
-      setRepos((prevRepos) => [...prevRepos, ...reposValidated]); // Adicionando os novos repositórios
+      setRepos((prevRepos) => [...prevRepos, ...reposValidated]);
       setLoading(false);
 
-      // Se não houver mais repositórios, definimos hasMore como false
       if (reposValidated.length < 10) {
         setHasMore(false);
       }
     } catch (err) {
-      console.error(err); // Logando o erro
+      console.error(err);
       setError(t("error.userNotFound"));
       setLoading(false);
     }
@@ -79,50 +108,188 @@ const Profile = () => {
   if (error) return <Text color="red.500">{error}</Text>;
 
   return (
-    <Box backgroundColor={'#FCFCFC'} >
-      {/* Cabeçalho com a logo e a SearchBar */}
+    <Box backgroundColor={'#FCFCFC'}>
       <Header />
 
-      {/* Informações do Usuário */}
-      <Stack marginTop={8}>
-        <Image src={userData?.avatar_url} alt={userData?.name} borderRadius="full" boxSize="150px" />
-        <Text fontSize="2xl">{userData?.name}</Text>
-        <Text>{userData?.bio}</Text>
-        {userData?.blog && (
-          <Link href={userData?.blog}>
-            <Button colorScheme="teal">{t("profile.visitWebsite")}</Button>
-          </Link>
-        )}
-        {userData?.twitter_username && (
-          <Link href={`https://twitter.com/${userData?.twitter_username}`}>
-            <Button colorScheme="twitter">{t("profile.visitTwitter")}</Button>
-          </Link>
-        )}
-      </Stack>
-
-      {/* Repositórios */}
-      <Box marginTop={8}>
-        <Text fontSize="lg" fontWeight="bold">{t("profile.repositories")}</Text>
-        <InfiniteScroll
-          dataLength={repos.length} // Tamanho dos repositórios já carregados
-          next={fetchMoreRepos} // Função para carregar mais
-          hasMore={hasMore} // Condição de quando há mais repositórios
-          loader={<Spinner size="xl" />}
-          endMessage={<Text>{t("profile.noMoreRepos")}</Text>} // Mensagem quando não há mais repositórios
+      <HStack
+        paddingBlock={'80px'}
+        paddingInline={'112px'}
+        display={'flex'}
+        justifyContent={'flex-start'}  // Ajustando para garantir que as colunas comecem do topo
+        alignItems={'flex-start'}  // Alinha ambas as colunas ao topo
+      >
+        {/* Primeira Coluna */}
+        <VStack
+          align="flex-start"
+          backgroundColor={'#FFFFFF'}
+          marginBottom={4}  // Ajusta o espaçamento entre os itens dentro da coluna
+          paddingBlock={'24px'}
+          paddingInline={'16px'}
+          borderRadius={'4px'}
         >
-          <Stack>
+          {/* Div para Imagem, Nome e @username lado a lado */}
+          <HStack gap={4}>
+            {/* Coluna para a Imagem */}
+            <Image
+              src={userData?.avatar_url}
+              alt={userData?.name}
+              borderRadius="full"
+              boxSize="48px"
+            />
+
+            {/* Coluna para o Nome e o @username */}
+            <VStack align="flex-start" >
+              <Text fontSize="20px" fontWeight="bold" color="black" marginBottom={'4px'}>
+                {userData?.name}
+              </Text>
+              <Text color='#A0AEC0' fontSize={'14px'} fontWeight={'normal'}>
+                @{userData?.login}
+              </Text>
+            </VStack>
+          </HStack>
+
+          {/* Bio */}
+          {userData?.bio && (
+            <Text color="#4A5568" marginBottom={'24px'} maxWidth={'248px'} fontSize={'16px'} marginTop={'16px'}>
+              {userData?.bio}
+            </Text>
+          )}
+
+          {/* Informações de seguidores e seguindo */}
+          <HStack>
+            <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'} >
+              <FollowersIcon width="24px" height="24px" />
+              <Text fontSize="lg" color="#4A5568" marginLeft={'8px'}>
+                {userData?.followers} {t("profile.followers")}
+              </Text>
+            </Box>
+          </HStack>
+          <HStack>
+            <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'}>
+              <Heart width="24px" height="24px" />
+              <Text fontSize="lg" color="#4A5568" marginLeft={'8px'}>
+                {userData?.following} {t("profile.following")}
+              </Text>
+            </Box>
+          </HStack>
+
+          {/* Informações adicionais */}
+          <VStack align="flex-start" marginTop={'24px'}>
+            {userData?.company &&
+              <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'}>
+                <Company width="24px" height="24px" />
+                <Text color="#4A5568" marginLeft={'8px'}>{userData.company}</Text>
+              </Box>}
+            {userData?.location &&
+              <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'}>
+                <Loc width="24px" height="24px" />
+                <Text color="#4A5568" marginLeft={'8px'}> {userData.location}</Text>
+              </Box>}
+            {userData?.email &&
+              <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'}>
+                <Email width="24px" height="24px" />
+                <Text color="#4A5568" marginLeft={'8px'}>{userData.email}</Text>
+              </Box>
+            }
+            {userData?.blog && (
+              <Link href={userData.blog} >
+                <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'}>
+                  <Site width="24px" height="24px" />
+                  <Button
+                    margin={'0px'}
+                    padding={'0px 0px'}
+                    color={'#4A5568'}
+                    title={userData.blog}
+                    marginLeft={'8px'}
+                  >
+                    {userData.blog}
+                  </Button>
+                </Box>
+              </Link>
+            )}
+            {userData?.twitter_username && (
+              <Link href={`https://twitter.com/${userData?.twitter_username}`} >
+                <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'}>
+                  <Twitter width="24px" height="24px" />
+                  <Button colorScheme="twitter">
+                    {t("profile.visitTwitter")}
+                  </Button>
+                </Box>
+              </Link>
+            )}
+          </VStack>
+
+          {/* Botão de Contato */}
+          <Button
+            colorScheme="blue"
+            width="full"
+            marginTop={4}
+            color={'white'}
+            backgroundColor={'#8C19D2'}
+            _hover={{
+              backgroundColor: "#7A14C2",
+              boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+            }}
+            transition="all 0.3s ease-in-out"
+          >
+            {t("profile.contact")}
+          </Button>
+        </VStack>
+
+        {/* Segunda Coluna (Repositórios) */}
+        <VStack
+          align="flex-start"
+          flex="1"
+          maxHeight="calc(100vh - 80px)"
+          overflow="auto"
+        >
+          <Text fontSize="lg" fontWeight="bold" color="black" marginBottom={4}>
+            {t("profile.repositories")}
+          </Text>
+
+          <InfiniteScroll
+            dataLength={repos.length}
+            next={fetchMoreRepos}
+            hasMore={hasMore}
+            loader={<Spinner size="xl" />}
+            endMessage={<Text color="black">{t("profile.noMoreRepos")}</Text>}
+          >
             {repos.map((repo, index) => (
-              <Box key={index} p={4} borderWidth={1} borderRadius="md">
+              <Box
+                key={index}
+                p={4}
+                borderWidth={1}
+                borderRadius="md"
+                width="full"
+                marginBottom={4}
+                _hover={{ backgroundColor: "#F7FAFC", cursor: "pointer" }}
+                boxShadow="0px 4px 6px rgba(0, 0, 0, 0.1)"
+              >
                 <Link href={repo.html_url}>
-                  <Text fontWeight="bold">{repo.name}</Text>
+                  <Text fontWeight="bold" fontSize="lg" color="#171923">
+                    {repo.name}
+                  </Text>
                 </Link>
-                <Text>{t("profile.stars")}: {repo.stargazers_count}</Text>
-                <Text>{t("profile.created")}: {format(new Date(repo.created_at), 'dd/MM/yyyy')}</Text>
+
+                <Text color="#4A5568" fontSize="sm" marginTop={2}>
+                  {repo.description || t("profile.noDescription")}
+                </Text>
+
+                <HStack gap={4} marginTop={4} color="#4A5568" justify="flex-start">
+                  <HStack>
+                    <StarIcon width="24px" height="24px" color="#F6AD55" />
+                    <Text>{repo.stargazers_count}</Text>
+                  </HStack>
+
+                  <HStack>
+                    <Text>{`${t("profile.updated")} ${formatDistanceToNow(new Date(repo.updated_at), { locale: ptBR })}`}</Text>
+                  </HStack>
+                </HStack>
               </Box>
             ))}
-          </Stack>
-        </InfiniteScroll>
-      </Box>
+          </InfiniteScroll>
+        </VStack>
+      </HStack>
     </Box>
   );
 };
